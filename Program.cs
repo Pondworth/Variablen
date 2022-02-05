331ca6ab474145f35d97edfe1b28e3467db82e04
Pondworth <amy@pondworth.de>
2022-02-05 14:00:20 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variablen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaration = Bekanntmachung einer Variable
+            string username;
+
+            //Initialisierung = Wertausgabe
+            username = "Amy";
+
+            //Deklaration und Initialisierung
+            //string username = "Amy";
+
+            Console.WriteLine(username);
+            Console.ReadKey();
+        }
+    }
+}
